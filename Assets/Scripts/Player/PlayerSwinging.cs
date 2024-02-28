fcb33e9bdaaaef0647bdd4326b8edda6bd828830
Iwan <77576646+CookieLover3000@users.noreply.github.com>
2024-02-28 16:07:31 +0100

    added a (bad) swinging implementation

---
+++ Assets/Scripts/Player/PlayerSwinging.cs
@@ -0,0 +1,126 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerSwinging : MonoBehaviour
+{
+    [Header("References")] 
+    public LineRenderer lr;
+    public Transform gunTip, cam, player;
+    public LayerMask whatIsGrappleable;
+    
+    [Header("Swinging")]
+    public float maxSwingDistance = 15f;
+    private Vector3 swingPoint;
+    private SpringJoint joint;
+    
+    public KeyCode swingKey = KeyCode.Mouse0;
+
+    [Header("Prediction")] 
+    public RaycastHit predictionHit;
+    public float predictionSphereCastRadius;
+    public Transform predicitionPoint;
+    
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(swingKey))
+            StartSwing();
+        if(Input.GetKeyUp(swingKey))
+            StopSwing();
+        
+        CheckForSwingPoints();
+    }
+
+    private void LateUpdate()
+    {
+        drawRope();
+    }
+
+    private void StartSwing()
+    {
+        // return if predictionHit not found
+        if (predictionHit.point == Vector3.zero) 
+            return;
+
+        swingPoint = predictionHit.point;
+        joint = player.gameObject.AddComponent<SpringJoint>();
+        joint.autoConfigureConnectedAnchor = false;
+        joint.connectedAnchor = swingPoint;
+
+        float distanceFromPoint = Vector3.Distance(player.position, swingPoint);
+
+        // the distance grapple will try to keep from grapple point. 
+        joint.maxDistance = distanceFromPoint * 0.8f;
+        joint.minDistance = distanceFromPoint * 0.25f;
+
+        // customize values as you like
+        joint.spring = 4.5f;
+        joint.damper = 7f;
+        joint.massScale = 4.5f;
+
+        lr.positionCount = 2;
+        currentGrapplePosition = gunTip.position;
+    }
+
+    void StopSwing()
+    {
+        lr.positionCount = 0;
+        Destroy(joint);
+    }
+
+    private Vector3 currentGrapplePosition;
+    void drawRope()
+    {
+        // if not grappling don't draw rope
+        if(!joint)
+            return;
+        
+        // make it so the line doesn't appear instantly
+        currentGrapplePosition = Vector3.Lerp(currentGrapplePosition, swingPoint, Time.deltaTime * 8f);
+        
+        lr.SetPosition(0, gunTip.position);
+        lr.SetPosition(1, swingPoint);
+    }
+
+    private void CheckForSwingPoints()
+    {
+        if(joint != null)
+           return;
+
+        RaycastHit sphereCastHit;
+        Physics.SphereCast(cam.position, predictionSphereCastRadius, cam.forward, out sphereCastHit, maxSwingDistance,
+            whatIsGrappleable);
+
+        RaycastHit raycastHit;
+        Physics.Raycast(cam.position, cam.forward, out raycastHit, maxSwingDistance, whatIsGrappleable);
+
+        Vector3 realHitPoint;
+
+        // direct hit
+        if (raycastHit.point != Vector3.zero)
+            realHitPoint = raycastHit.point;
+        
+        // indirect (predicted) hit
+        else if (sphereCastHit.point != Vector3.zero)
+            realHitPoint = sphereCastHit.point;
+        else
+            realHitPoint = Vector3.zero;
+
+        if (realHitPoint != Vector3.zero)
+        {
+            predicitionPoint.gameObject.SetActive(true);
+            predicitionPoint.position = realHitPoint;
+        }
+        else 
+            predicitionPoint.gameObject.SetActive(false);
+
+        predictionHit = raycastHit.point == Vector3.zero ? sphereCastHit : raycastHit;
+        
+        
+    }
+}
