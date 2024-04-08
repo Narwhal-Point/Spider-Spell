d84144578df26170e70fd2c4bb8de438a6701372
Iwan <iwanvanoort@gmail.com>
2024-04-08 17:06:38 +0200

    camera feels a lot better now

--- Assets/Scripts/Player/PlayerCam.cs
+++ Assets/Scripts/Player/PlayerCam.cs
@@ -13,7 +13,7 @@ namespace Player
         [SerializeField] private PlayerMovement _playerMovement;
         [SerializeField] private Transform combatLookAt;
 
-        [SerializeField] private CinemachineVirtualCameraBase[] cameras;
+        [SerializeField] private CinemachineFreeLook[] cameras;
         [SerializeField] private GameObject crosshair;
         
         public enum CameraStyle
@@ -23,7 +23,7 @@ namespace Player
         }
 
         public CameraStyle CurrentCamera { get; private set; } = CameraStyle.Normal;
-
+        private CameraStyle PrevCamera { get; set; } = CameraStyle.Normal;
 
         // Start is called before the first frame update
         void Start()
@@ -40,6 +40,13 @@ namespace Player
 
             if (CurrentCamera == CameraStyle.Normal)
             {
+                if (PrevCamera == CameraStyle.Aiming)
+                {
+                    cameras[0].m_YAxis = cameras[1].m_YAxis;
+                    cameras[0].m_XAxis = cameras[1].m_XAxis;
+                    PrevCamera = CameraStyle.Normal;
+                }
+
                 cameras[1].gameObject.SetActive(false);
                 cameras[0].gameObject.SetActive(true);
                 if (crosshair.activeSelf)
@@ -48,6 +55,13 @@ namespace Player
 
             else if (CurrentCamera == CameraStyle.Aiming)
             {
+                if (PrevCamera == CameraStyle.Normal)
+                {
+                    cameras[1].m_YAxis = cameras[0].m_YAxis;
+                    cameras[1].m_XAxis = cameras[0].m_XAxis;
+                    PrevCamera = CameraStyle.Aiming;
+                }
+
                 if (!crosshair.activeSelf)
                     crosshair.SetActive(true);
                 cameras[0].gameObject.SetActive(false);
