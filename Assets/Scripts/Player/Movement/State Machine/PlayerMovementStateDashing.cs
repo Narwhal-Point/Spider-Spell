bffa4a3b9d422fb3fcae24ecf0d0b58fc38f2591
Iwan <iwanvanoort@gmail.com>
2024-04-09 17:09:26 +0200

    implemented dashing (NP-173)

--- Assets/Scripts/Player/Movement/State Machine/PlayerMovementStateDashing.cs
+++ Assets/Scripts/Player/Movement/State Machine/PlayerMovementStateDashing.cs
@@ -4,26 +4,60 @@ namespace Player.Movement.State_Machine
 {
     public class PlayerMovementStateDashing : PlayerMovementBaseState
     {
+        private float _dashDuration;
+        private float _dashForce;
+        private float _dashUpwardForce;
+        private float _dashCooldown;
+
         private float _dashTimer;
         
-        public PlayerMovementStateDashing(PlayerMovementStateManager manager, PlayerMovement player) : base(manager, player)
+
+        private float lastDashTime = 2f;
+        public PlayerMovementStateDashing(PlayerMovementStateManager manager, PlayerMovement player, float dashDuration, float dashForce, float dashCooldown, float dashUpwardForce) : base(manager,
+            player)
         {
+            _dashDuration = dashDuration;
+            _dashForce = dashForce;
+            _dashCooldown = dashCooldown;
+            _dashUpwardForce = dashUpwardForce;
         }
 
 
         public override void EnterState()
         {
-            _dashTimer = player.dashDuration;
-            player.Rb.AddForce(player.playerObj.transform.forward * player.dashForce, ForceMode.Impulse);
+            Debug.Log(Time.time - lastDashTime);
+            if (Time.time - lastDashTime <= _dashCooldown)
+            {
+                manager.SwitchState(player.FallingState);
+                return;
+            }
+            
+            player.Rb.drag = 0f;
+            player.Rb.useGravity = true;
+            player.IsDashing = true;
+            _dashTimer = _dashDuration;
+            lastDashTime = Time.time;
+            Vector3 forceToApply = player.orientation.forward * _dashForce + player.orientation.up * _dashUpwardForce;
+
+            player.Rb.AddForce(forceToApply, ForceMode.Impulse);
+            
         }
+
+        private void ResetDash()
+        {
+            player.IsDashing = false;
+            // player.Rb.velocity = Vector3.zero;
+        }
+
 
         public override void UpdateState()
         {
-            Debug.Log(player.Rb.drag);
             _dashTimer -= Time.deltaTime;
-            
-            if(_dashTimer <= 0f)
-                manager.SwitchState(player.IdleState);
+            if (_dashTimer <= 0f)
+            {
+                ResetDash();
+                manager.SwitchState(player.FallingState);
+            }
         }
     }
 }
