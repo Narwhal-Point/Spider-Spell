53489efb95564b1adf4cb66e9ef68fda8ebfc137
AbrahamLincoln <s1136731@student.hsleiden.nl>
2024-05-09 21:30:24 +0200

    Changed the text from legacy to TMPro

--- Assets/Samples/Input System/1.7.0/Rebinding UI/KeyBoardMouseIconsExample.cs
+++ Assets/Samples/Input System/1.7.0/Rebinding UI/KeyBoardMouseIconsExample.cs
@@ -74,7 +74,7 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
  [Serializable]
 public struct KeyboardIcons
 {
-    // Define sprites for keyboard keys
+    // Alphabets
     public Sprite keyA;
     public Sprite keyB;
     public Sprite keyC;
@@ -101,6 +101,8 @@ public struct KeyboardIcons
     public Sprite keyX;
     public Sprite keyY;
     public Sprite keyZ;
+
+    // Numerals
     public Sprite key0;
     public Sprite key1;
     public Sprite key2;
@@ -111,6 +113,8 @@ public struct KeyboardIcons
     public Sprite key7;
     public Sprite key8;
     public Sprite key9;
+
+    // Function keys
     public Sprite keyF1;
     public Sprite keyF2;
     public Sprite keyF3;
@@ -123,74 +127,115 @@ public struct KeyboardIcons
     public Sprite keyF10;
     public Sprite keyF11;
     public Sprite keyF12;
+
+    // Modifier keys
     public Sprite keyCtrl;
     public Sprite keyShift;
     public Sprite keyAlt;
-    // Add more sprites for other keys as needed
+    public Sprite keyWin;
+    public Sprite keySpaceBar;
+
+    // Special characters
+    public Sprite keyDot;
+    public Sprite keyComma;
+    public Sprite keyQuestionMark;
+    public Sprite keyApostrophe;
+    public Sprite keyDoubleQuote;
+    public Sprite keyBacktick; // Also known as Grave accent or Grave
+    public Sprite keyUnderscore;
+
+    // Navigation keys
+    public Sprite keyArrowLeft;
+    public Sprite keyArrowRight;
+    public Sprite keyArrowUp;
+    public Sprite keyArrowDown;
 
     public Sprite GetSprite(string controlPath)
+{
+    // Map control path to keyboard keys
+    switch (controlPath)
     {
-        // Map control path to keyboard keys
-        switch (controlPath)
-        {
-            case "a": return keyA;
-            case "b": return keyB;
-            case "c": return keyC;
-            case "d": return keyD;
-            case "e": return keyE;
-            case "f": return keyF;
-            case "g": return keyG;
-            case "h": return keyH;
-            case "i": return keyI;
-            case "j": return keyJ;
-            case "k": return keyK;
-            case "l": return keyL;
-            case "m": return keyM;
-            case "n": return keyN;
-            case "o": return keyO;
-            case "p": return keyP;
-            case "q": return keyQ;
-            case "r": return keyR;
-            case "s": return keyS;
-            case "t": return keyT;
-            case "u": return keyU;
-            case "v": return keyV;
-            case "w": return keyW;
-            case "x": return keyX;
-            case "y": return keyY;
-            case "z": return keyZ;
-            case "0": return key0;
-            case "1": return key1;
-            case "2": return key2;
-            case "3": return key3;
-            case "4": return key4;
-            case "5": return key5;
-            case "6": return key6;
-            case "7": return key7;
-            case "8": return key8;
-            case "9": return key9;
-            case "f1": return keyF1;
-            case "f2": return keyF2;
-            case "f3": return keyF3;
-            case "f4": return keyF4;
-            case "f5": return keyF5;
-            case "f6": return keyF6;
-            case "f7": return keyF7;
-            case "f8": return keyF8;
-            case "f9": return keyF9;
-            case "f10": return keyF10;
-            case "f11": return keyF11;
-            case "f12": return keyF12;
-            case "leftCtrl": 
-            case "rightCtrl": return keyCtrl;
-            case "leftShift": 
-            case "rightShift": return keyShift;
-            case "leftAlt": 
-            case "rightAlt": return keyAlt;
-            // Add mappings for other keys
-            default: return null;
-        }
+        // Alphabets
+        case "a": return keyA;
+        case "b": return keyB;
+        case "c": return keyC;
+        case "d": return keyD;
+        case "e": return keyE;
+        case "f": return keyF;
+        case "g": return keyG;
+        case "h": return keyH;
+        case "i": return keyI;
+        case "j": return keyJ;
+        case "k": return keyK;
+        case "l": return keyL;
+        case "m": return keyM;
+        case "n": return keyN;
+        case "o": return keyO;
+        case "p": return keyP;
+        case "q": return keyQ;
+        case "r": return keyR;
+        case "s": return keyS;
+        case "t": return keyT;
+        case "u": return keyU;
+        case "v": return keyV;
+        case "w": return keyW;
+        case "x": return keyX;
+        case "y": return keyY;
+        case "z": return keyZ;
+
+        // Numerals
+        case "1": return key1;
+        case "2": return key2;
+        case "3": return key3;
+        case "4": return key4;
+        case "5": return key5;
+        case "6": return key6;
+        case "7": return key7;
+        case "8": return key8;
+        case "9": return key9;
+        case "0": return key0;
+
+        // Function keys
+        case "f1": return keyF1;
+        case "f2": return keyF2;
+        case "f3": return keyF3;
+        case "f4": return keyF4;
+        case "f5": return keyF5;
+        case "f6": return keyF6;
+        case "f7": return keyF7;
+        case "f8": return keyF8;
+        case "f9": return keyF9;
+        case "f10": return keyF10;
+        case "f11": return keyF11;
+        case "f12": return keyF12;
+
+        // Modifier keys
+        case "ctrl": return keyCtrl;
+        case "shift": return keyShift;
+        case "alt": return keyAlt;
+        case "win": return keyWin;
+
+        // Special characters
+        case "period": return keyDot;
+        case "comma": return keyComma;
+        case "questionmark": return keyQuestionMark;
+        case "apostrophe": return keyApostrophe;
+        case "doublequote": return keyDoubleQuote;
+        case "backtick": return keyBacktick;
+        case "underscore": return keyUnderscore;
+        case "space": return keySpaceBar;
+
+        // Navigation keys
+        case "leftArrow": return keyArrowLeft;
+        case "upArrow": return keyArrowUp;
+        case "downArrow": return keyArrowDown;
+        case "rightArrow": return keyArrowRight;
+
+        default:
+            return null;
     }
+}
+
 }
 
 
