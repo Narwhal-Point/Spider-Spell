100848dbc7389c74cd663b5b64f58d1dafb3ce6f
Iwan <77576646+CookieLover3000@users.noreply.github.com>
2024-05-09 15:11:17 +0200

    removed the jump animation and added movement jumping

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -475,13 +475,14 @@ namespace Player.Movement
 
         private bool isJumping = false;
 
-        public void OnJump()
+        public void OnJump(InputValue value)
         {
+            isJumping = value.isPressed;
             if (!isJumping && (_manager.CurrentState == IdleState || _manager.CurrentState == WalkingState))
             {
-                isJumping = true;
-                jumpAnimation = true;
-                StartCoroutine(DelayedStateSwitch(JumpingState, .5f)); // 50 frames delay
+                // isJumping = true;
+                _manager.SwitchState(JumpingState);
+                // StartCoroutine(DelayedStateSwitch(JumpingState, .5f)); // 50 frames delay
             }
         }
 
