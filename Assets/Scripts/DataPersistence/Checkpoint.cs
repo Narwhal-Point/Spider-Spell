bae2a48e33d9770d14f6bd8ba36977987f4bd71c
Kalong98 <68122498+Kalong98@users.noreply.github.com>
2024-05-22 19:14:31 +0200

    Checkpoint particles + SFX

--- Assets/Scripts/DataPersistence/Checkpoint.cs
+++ Assets/Scripts/DataPersistence/Checkpoint.cs
@@ -4,10 +4,21 @@ namespace DataPersistence
 {
     public class Checkpoint : MonoBehaviour//, IDataPersistence
     {
+        ParticleSystem starsVFX;
+        AudioSource magicSFX;
+
+        private void Start()
+        {
+            starsVFX = gameObject.GetComponent<ParticleSystem>();
+            magicSFX = gameObject.GetComponent<AudioSource>();
+        }
+
         void OnTriggerEnter(Collider collider)
         {
             Debug.Log("test");
             DataPersistenceManager.instance.SaveGame();
+            starsVFX.Play();
+            magicSFX.Play();
         }
     }
 }
