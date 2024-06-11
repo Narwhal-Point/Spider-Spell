6710f6b681bf3268e7a7faefa9aca1263193fb44
Iwan <77576646+CookieLover3000@users.noreply.github.com>
2024-06-11 14:12:08 +0200

    renamed some variables

--- Assets/Scripts/Objects/Fire.cs
+++ Assets/Scripts/Objects/Fire.cs
@@ -1,97 +1,97 @@
 using Player;
-using Player.Movement;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Firebeam : MonoBehaviour
+namespace Objects
 {
-    [SerializeField] float maxHeightMultiplier = 2;
-    [SerializeField] float riseSpeed = 3;
-    [SerializeField] float upTime = 5;
-    [SerializeField] float downTime = 5;
-    [SerializeField] float calmGravity = -3;
-    [SerializeField] float heavyGravity = -20;
-    [SerializeField] ParticleSystem fireVFX;
-
-    private Vector3 startingScale;
-    private float timer;
-    private bool goingUp;
-    private bool goingDown;
-    private bool stayUp;
-    private bool coolingDown;
-    private float initialHeight;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Firebeam : MonoBehaviour
     {
-        goingUp = false;
-        goingDown = false;
-        stayUp = false;
-        coolingDown = true;
-        timer = 0;
-        startingScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
-        initialHeight = startingScale.y;
-    }
+        [SerializeField] private float maxHeightMultiplier = 2;
+        [SerializeField] private float riseSpeed = 3;
+        [SerializeField] private float upTime = 5;
+        [SerializeField] private float downTime = 5;
+        [SerializeField] private float calmGravity = -3;
+        [SerializeField] private float heavyGravity = -20;
+        [SerializeField] private ParticleSystem fireVFX;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (goingUp)
+        private Vector3 _startingScale;
+        private float _timer;
+        private bool _goingUp;
+        private bool _goingDown;
+        private bool _stayUp;
+        private bool _coolingDown;
+        private float _initialHeight;
+
+        // Start is called before the first frame update
+        private void Start()
         {
-            float riseValue = riseSpeed * Time.deltaTime;
-            transform.localScale = new Vector3(startingScale.x, transform.localScale.y + riseValue, startingScale.z);
+            _goingUp = false;
+            _goingDown = false;
+            _stayUp = false;
+            _coolingDown = true;
+            _timer = 0;
+            _startingScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            _initialHeight = _startingScale.y;
+        }
 
-            if (transform.localScale.y / initialHeight >= maxHeightMultiplier)
+        // Update is called once per frame
+        private void Update()
+        {
+            if (_goingUp)
             {
-                goingUp = false;
-                stayUp = true;
+                float riseValue = riseSpeed * Time.deltaTime;
+                transform.localScale = new Vector3(_startingScale.x, transform.localScale.y + riseValue, _startingScale.z);
+
+                if (transform.localScale.y / _initialHeight >= maxHeightMultiplier)
+                {
+                    _goingUp = false;
+                    _stayUp = true;
+                }
+            }
+            else if (_stayUp)
+            {
+                _timer += Time.deltaTime;
+                if (_timer > upTime)
+                {
+                    _stayUp = false;
+                    _goingDown = true;
+                    fireVFX.gravityModifier = calmGravity;
+                    _timer = 0;
+                }
+            }
+            else if (_goingDown)
+            {
+                float fallValue = riseSpeed * Time.deltaTime;
+                transform.localScale = new Vector3(_startingScale.x, transform.localScale.y - fallValue, _startingScale.z);
+
+                if (transform.localScale.y <= _initialHeight)
+                {
+                    _goingDown = false;
+                    _coolingDown = true;
+                    transform.localScale = new Vector3(_startingScale.x, _initialHeight, _startingScale.z);
+                }
+            }
+            else if (_coolingDown)
+            {
+                _timer += Time.deltaTime;
+                if (_timer > downTime)
+                {
+                    _coolingDown = false;
+                    _goingUp = true;
+                    fireVFX.gravityModifier = heavyGravity;
+                    _timer = 0;
+                }
             }
         }
-        else if (stayUp)
-        {
-            timer += Time.deltaTime;
-            if (timer > upTime)
-            {
-                stayUp = false;
-                goingDown = true;
-                fireVFX.gravityModifier = calmGravity;
-                timer = 0;
-            }
-        }
-        else if (goingDown)
-        {
-            float fallValue = riseSpeed * Time.deltaTime;
-            transform.localScale = new Vector3(startingScale.x, transform.localScale.y - fallValue, startingScale.z);
 
-            if (transform.localScale.y <= initialHeight)
-            {
-                goingDown = false;
-                coolingDown = true;
-                transform.localScale = new Vector3(startingScale.x, initialHeight, startingScale.z);
-            }
-        }
-        else if (coolingDown)
+        private void OnTriggerEnter(Collider other)
         {
-            timer += Time.deltaTime;
-            if (timer > downTime)
+            Debug.Log("shit");
+            if (other.gameObject.CompareTag("Player"))
             {
-                coolingDown = false;
-                goingUp = true;
-                fireVFX.gravityModifier = heavyGravity;
-                timer = 0;
+                // Debug.Log("blowing");
+                PlayerDeathManager deathManager = other.gameObject.GetComponent<PlayerDeathManager>();
+                deathManager.KillPlayer();
             }
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("shit");
-        if (other.gameObject.CompareTag("Player"))
-        {
-            // Debug.Log("blowing");
-            PlayerDeathManager deathManager = other.gameObject.GetComponent<PlayerDeathManager>();
-            deathManager.KillPlayer();
         }
     }
 }
