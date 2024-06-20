5a7bacdbf1f5013ff9665a388627567239a47f73
Iwan <77576646+CookieLover3000@users.noreply.github.com>
2024-06-20 20:08:35 +0200

    added new cauldron + shortened start cutscene

--- Assets/Scripts/Cutscene/startCutscene.cs
+++ Assets/Scripts/Cutscene/startCutscene.cs
@@ -8,22 +8,15 @@ namespace Cutscene
     public class startCutscene : MonoBehaviour
     {
         [SerializeField] private PlayableDirector playableDirector;
-        [SerializeField] private SetTextToTextBox continueCutsceneText;
-        public bool Started { private get; set;  }
-
-        private void Start()
-        {
-            continueCutsceneText.SetText("");
-        }
+        public bool Started { private get; set; }
 
         private void Update()
         {
             if (playableDirector.state == PlayState.Paused && Started)
             {
-                continueCutsceneText.SetText("Press [Interact] to continue");
                 // yes, this is not optimal. idc
-                InputManager.instance.EnableInteract();   
-                if(InputManager.instance.InteractInput)
+                InputManager.instance.EnableInteract();
+                if (InputManager.instance.InteractInput)
                     playableDirector.Resume();
             }
         }
